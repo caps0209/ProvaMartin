46c143286f1b2c22b0cf470933c71d7998b9409a
João Luiz <synch0209@gmail.com>
2020-10-29 00:22:10 -0300

    Criado teste de inserir, + polimento dos testes ja criados

--- src/testes/TestsPessoaDAO.java
+++ src/testes/TestsPessoaDAO.java
@@ -1,4 +1,5 @@
 package testes;
+
 import static org.junit.Assert.*;
 import java.sql.Connection;
 import java.sql.SQLException;
@@ -9,20 +10,15 @@ import com.db.ConnHSQLDB;
 import com.entidades.Pessoa;
 import dao.PessoaDAO;
 
-
 public class TestsPessoaDAO {
 
 	PessoaDAO p;
+	Pessoa a;
 	Connection connection;
-	
+
 	@Before
 	public void setUp() throws Exception {
 		p = new PessoaDAO();
-	}
-
-	@Test
-	public void testInserir() {
-		fail("Not yet implemented");
 	}
 
 	@Test
@@ -36,12 +32,22 @@ public class TestsPessoaDAO {
 	}
 
 	@Test
+	public void testConectar() {
+		connection = new ConnHSQLDB().conectar();
+		try {
+			assertTrue(connection.isValid(1));
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+	}
+
+	@Test
 	public void testListAll() {
 		List<Pessoa> listaPessoa = this.p.listAll();
 		assertEquals(2, listaPessoa.size());
 		assertEquals("pedro", listaPessoa.get(0).getNome());
 		assertEquals("joao", listaPessoa.get(1).getNome());
-		//assertEquals("jose", listaPessoa.get(2).getNome());
+		// assertEquals("jose", listaPessoa.get(2).getNome());
 	}
 
 	@Test
@@ -51,15 +57,22 @@ public class TestsPessoaDAO {
 		System.out.println(pessoa.getNome());
 	}
 
+	
 	@Test
-	public void testConectar(){
-		connection = new ConnHSQLDB().conectar();
-		try {
-			assertTrue(connection.isValid(1));
-		} catch (SQLException e) {
-			e.printStackTrace();
-		}
-		
-	}
+	public void testInserir() {
+		Pessoa pessoa = new Pessoa();
+		pessoa.setId(14);
+		pessoa.setNome("Igor");
+		pessoa.setDataNasc(null);
+		pessoa.setPeso(45);
 
+		System.out.println(pessoa.getId());
+		System.out.println(pessoa.getNome());
+		System.out.println(pessoa.getDataNasc());
+		System.out.println(pessoa.getPeso());
+
+		assertEquals("Sucesso ao add na tabela do banco", 1, p.inserir(pessoa));
+
+	}
+	 
 }
